cb909331bcacc408f02aa8723b820c2617f22430
silasvivid-sudo <silasvivid@gmail.com>
2025-11-01 13:56:50 +0800

    Update index.src.js

--- index.src.js
+++ index.src.js
@@ -20,7 +20,7 @@ const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLI
 const ARGO_DOMAIN = process.env.HOST || '';          // 固定隧道域名,留空即启用临时隧道
 const ARGO_AUTH = process.env.DATA || '';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
-const CFIP = process.env.GOODIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
+const CFIP = process.env.GOODIP || '194.53.53.7';        // 节点优选域名或优选ip  
 const CFPORT = process.env.GOODPORT || 443;                   // 节点优选域名或优选ip对应的端口
 const NAME = process.env.NAME || '';                        // 节点名称
 
